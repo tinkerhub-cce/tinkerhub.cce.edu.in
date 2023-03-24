728006cd6ed68433be3b5b26ce2ee699eb121147
sai-prasad-1 <sai.prasad.1603@gmail.com>
2023-03-24 14:21:43 +0530

    dockerize the app

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
